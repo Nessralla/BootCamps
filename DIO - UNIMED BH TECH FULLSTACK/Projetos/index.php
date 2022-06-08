27e5140685709c0230c51848374820f2120baff2
Nessralla <alexandre.nessralla@gmail.com>
2022-06-08 15:10:43 -0300

    Flexbox Intro

--- DIO - UNIMED BH TECH FULLSTACK/Projetos/index.php
+++ DIO - UNIMED BH TECH FULLSTACK/Projetos/index.php
@@ -2,11 +2,19 @@
     <heah>
     <title>Meu primeiro site em PHP! Woohoo!</title>
 
+    <style type="text/css">
+        .linha {
+            font-weight: bold;
+            color: green;
+            padding-left: 10px;
+            line-height: 50px;
+        }
+    </style>
     </head>
     <body>
         <?php
             for ($i = 0 ; $i < 10 ; $i++) {
-                print( "Linha número " . $i . "<br />");
+                print( " <span class=\"linha\"Linha número " . $i . "</span><br />");
             }
         ?>
     </body>
