954d9e52629908d45c4086eeeb15f574801de894
Nessralla <alexandre.nessralla@gmail.com>
2022-07-12 12:33:57 -0300

    instalaçao TypeScript

---
+++ DIO - UNIMED BH TECH FULLSTACK/Aulas/TypeScript/Basico.ts
@@ -0,0 +1,18 @@
+/* Aprendizado sobre TypeScript
+
+TypeScript é uma extensão do JavaScript ! 
+    Adiciona features à linguagem original
+    Compila para JS
+
+Vantagens:
+    - Definição estática de tipos:
+        + Funcionalidades mais explícitas
+        + Maior segurança durante o desenvolvimento
+        + Detecta erros durante a compilação
+    - Compila um JavaScript mais resiliente
+    - Ajuda a desenvolver boas práticas de programação
+
+    
+
+*/
+
